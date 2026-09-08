7ecb70b844e2843debde4add8f46f995a9aefbc8
testbros12345 <testbros12345@gmail.com>
2026-09-08 10:46:37 +0900

    Delete 11_チャット directory

--- 11_チャット/js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = 'https://thasfmwbnishvocfzlsd.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_hvf89jood_zaC2A_UcUtvw_4iZVoCnk';
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
